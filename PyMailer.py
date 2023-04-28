914bd28df14ac54c0dd707781fc37938cfba8c58
Rio Agung Purnomo <91432414+rioagungpurnomo@users.noreply.github.com>
2023-04-28 16:03:02 +0700

    Add files via upload

---
+++ PyMailer.py
@@ -0,0 +1,26 @@
+import smtplib
+from email.mime.text import MIMEText
+
+class PyMailer:
+  def __init__(self, array):
+    self.smtp_host = array['smtp_host']
+    self.smtp_port = array['smtp_port']
+    self.email = array['email']
+    self.password = array['password']
+    self.to = array['to']
+    self.subject = array['subject']
+    self.body = array['body']
+    self.display = array['display']
+    
+  def send(self):
+    msg = MIMEText(self.body, 'html')
+    msg['From'] = self.email
+    msg['To'] = self.to
+    msg['Subject'] = self.subject
+    
+    with smtplib.SMTP(self.smtp_host, self.smtp_port) as smtp:
+      smtp.starttls()
+      smtp.login(self.email, self.password)
+      smtp.send_message(msg)
+      if self.display:
+        print('Email sent successfully.')
