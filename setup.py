f3169296545207d6016425b7a7ecbb9632e6c8a6
rioagungpurnomo <rioagungpurnomo.ak@gmail.com>
2023-04-29 14:31:38 +0700

    Push

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+setup(
+    name='pymailers',
+    version='1.0.0',
+    description='The classic email sending library for Python.',
+    author='Rio Agung Purnomo',
+    author_email='rioagungpurnomo.ak@gmail.com',
+    url='https://github.com/rioagungpurnomo/pymailers',
+    packages=['pymailers'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+    ],
+)
