cd1bf27cb172acb78144921afc72f42fa79a7242
rioagungpurnomo <rioagungpurnomo.ak@gmail.com>
2023-05-01 00:06:45 +0700

    Update

--- pymailers/PyMailer.py
+++ pymailers/PyMailer.py
@@ -1,5 +1,5 @@
 import smtplib
-from sys import exit as logout
+import sys
 from email.mime.text import MIMEText
 
 class PyMailer:
@@ -12,17 +12,6 @@ class PyMailer:
     self.subject = array['subject']
     self.body = array['body']
     self.display = array['display']
-    self.WHITE = '\x1b[1;97m'
-    self.RED = '\x1b[1;91m'
-    self.GREEN = '\x1b[1;92m'
-    self.YELLOW = '\x1b[1;93m'
-    self.PURPLE = '\x1b[1;94m'
-    self.BLUE = '\x1b[1;96m'
-    self.DEFAULT = '\x1b[0m'
-    self.success = f"{self.WHITE}[{self.GREEN}!{self.WHITE}]"
-    self.failed = f"{self.WHITE}[{self.RED}!{self.WHITE}]"
-    self.message_smtp = "SMTP Host and SMTP Port error occurred."
-    self.message_passwprd = "SMTP Host and SMTP Port error occurred."
     
   def send(self):
     msg = MIMEText(self.body, 'html')
@@ -34,17 +23,19 @@ class PyMailer:
       with smtplib.SMTP(self.smtp_host, self.smtp_port) as smtp:
         smtp.starttls()
     except Exception:
-      logout(print(f"""\n{self.failed} {self.message_smtp}{self.DEFAULT}"""))
+      print("SMTP Host and SMTP Port error occurred.")
+      sys.exit()
     try:
         smtp.login(self.email, self.password)
         smtp.send_message(msg)
         if self.display:
-          print(f"""\n{self.WHITE}SMTP Host : {self.smtp_host}
+          print(f"""SMTP Host : {self.smtp_host}
           SMTP Port : {self.smtp_port}
           Email : {self.email}
           To : {self.to}
           Subject : {self.subject}
           Body : {self.body}\n
-          {self.success} Email sent successfully.{self.DEFAULT}""")
+          Email sent successfully.""")
     except Exception:
-      logout(print(f"""\n{self.failed} {self.message}{self.DEFAULT}"""))
+      ep = "if the email is correct, your password is wrong."
+      print(f"Check your email and password again, {ep}")
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='pymailers',
-    version='1.1.7',
+    version='1.1.8',
     description='The classic email sending library for Python.',
     long_description=long_description,
     long_description_content_type="text/markdown",
